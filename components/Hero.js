46e3741bcfe3ca319df23b09d5ec3ff0a5d7e284
Jason-Kyle De Lara <jason8oh8@gmail.com>
2021-07-12 07:09:22 -0700

    adjusted Hero component's css

--- components/Hero.js
+++ components/Hero.js
@@ -4,11 +4,11 @@ import styles from "../styles/Hero.module.css";
 export default function Hero() {
     return (
         <div className={styles.container}>
-            <img className={styles.heroImage} src="hero.jpg" />
-            <div>
-                <h1 className={styles.title}>One life. No Regrets.</h1>
-                <h3 className={styles.subtitle}>Make the best of it.</h3>
-            </div>
+            <div className={styles.divImage}></div>
+            {/* <img className={styles.heroImage} src="hero.jpg" /> */}
+            <h1 className={styles.title}>One life. No Regrets.</h1>
+            <h3 className={styles.subtitle}>Make the best of it.</h3>
+
             <div className={styles.subscribe}>
                 <input type="text" placeholder="EMAIL" />
                 <button>SUBSCRIBE</button>
