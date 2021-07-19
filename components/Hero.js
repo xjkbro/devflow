f17f5e7c7db593bfe653d39486b4cd6ed2e2459a
Jason-Kyle De Lara <jason8oh8@gmail.com>
2021-07-18 17:20:21 -0700

    convert styled comp

--- components/Hero.js
+++ components/Hero.js
@@ -1,20 +1,104 @@
 import styles from "../styles/Hero.module.css";
 import Subscribe from "../components/Subscribe";
+import styled from "styled-components";
 // import Image from "next/image";
+const Container = styled.div`
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    max-width: 100%;
+    height: 100%;
+    min-height: 1080px;
+    @media (max-width: 1400px) {
+        min-height: 900px;
+    }
+    @media (max-width: 1200px) {
+        min-height: 800px;
+    }
+    @media (max-width: 1000px) {
+        min-height: 700px;
+    }
+    @media (max-width: 700px) {
+        min-height: 500px;
+    }
+    @media (max-width: 1400px) {
+        min-height: 300px;
+    }
+`;
+const ImageDiv = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: url("hero.jpg");
+    background-repeat: no-repeat;
+    background-size: 100% auto;
+    mix-blend-mode: color-dodge;
+`;
+const Title = styled.h1`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    padding-top: 26.5%;
+    text-align: center;
+    color: white;
+    font-size: 4rem;
+    font-weight: 900;
+    text-transform: uppercase;
+    text-shadow: 5px 5px 15px #000;
+    letter-spacing: 10px;
+    @media (max-width: 1400px) {
+        font-size: 2.7rem;
+    }
+    @media (max-width: 700px) {
+        font-size: 1.6rem;
+    }
+    @media (max-width: 450px) {
+        font-size: 1.2rem;
+        letter-spacing: 5px;
+        padding-top: 15.5%;
+    }
+`;
+const SubTitle = styled.h3`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    padding-top: 35.5%;
+    text-align: center;
+    color: white;
+    font-size: 2rem;
+    font-weight: 300;
+    text-shadow: 5px 5px 15px #000;
+    letter-spacing: 10px;
+    @media (max-width: 1200px) {
+        font-size: 1.5rem;
+    }
+    @media (max-width: 700px) {
+        font-size: 1.12rem;
+    }
+    @media (max-width: 450px) {
+        font-size: 0.8rem;
+        letter-spacing: 2px;
+        padding-top: 25.5%;
+    }
+`;
 
 export default function Hero() {
     return (
-        <div className={styles.container}>
-            <div className={styles.divImage}></div>
+        <Container>
+            <ImageDiv />
             {/* <img className={styles.heroImage} src="hero.jpg" /> */}
-            <h1 className={styles.title}>One life. No Regrets.</h1>
-            <h3 className={styles.subtitle}>Make the best of it.</h3>
+            <Title>One life. No Regrets.</Title>
+            <SubTitle>Make the best of it.</SubTitle>
 
             <Subscribe />
             {/* <div className={styles.subscribe}>
                 <input type="text" placeholder="EMAIL" />
                 <button>SUBSCRIBE</button>
             </div> */}
-        </div>
+        </Container>
     );
 }
--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,155 @@ import sanityClient from "../utils/client";
 import { useEffect, useState } from "react";
 import { SocialIcon } from "react-social-icons";
 import styles from "../styles/Contact.module.css";
+import styled from "styled-components";
 import Head from "next/head";
+
+const Halves = styled.div`
+    height: 300px;
+    width: 100%;
+    display: inline-flex;
+`;
+const Email = styled.div`
+    height: 100%;
+    width: 50%;
+    background-color: #85b1cc;
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    justify-content: center;
+    padding: 40px;
+    transition: all 150ms ease-in;
+    > h1 {
+        font-weight: 900;
+        font-size: 2rem;
+    }
+    > h5 {
+        font-size: 1.1rem;
+        /* font-weight: 300; */
+    }
+    > p {
+        padding-bottom: 20px;
+        font-size: 1.5rem;
+        font-weight: 300;
+        /* text-transform: uppercase; */
+    }
+    :hover {
+        filter: grayscale(100%);
+    }
+    @media (max-width: 700px) {
+        > h1 {
+            font-weight: 900;
+            font-size: 1.5rem;
+        }
+        > h5 {
+            font-size: 0.7rem;
+            /* font-weight: 300; */
+        }
+        > p {
+            padding-bottom: 20px;
+            font-size: 1rem;
+            font-weight: 300;
+            /* text-transform: uppercase; */
+        }
+    }
+`;
+const Sponsors = styled.div`
+    height: 100%;
+    width: 50%;
+    background-color: #667a86;
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    justify-content: center;
+    padding: 40px;
+    transition: all 150ms ease-in;
+    > h1 {
+        font-weight: 900;
+        font-size: 2rem;
+    }
+    > p {
+        padding-bottom: 50px;
+        font-size: 1.5rem;
+        font-weight: 300;
+        /* text-transform: uppercase; */
+    }
+    :hover {
+        filter: grayscale(100%);
+    }
+    :hover::after {
+        background-color: rgba(#000, 0.5);
+    }
+    @media (max-width: 700px) {
+        > h1 {
+            font-weight: 900;
+            font-size: 1.5rem;
+        }
+        > p {
+            padding-bottom: 50px;
+            font-size: 0.7rem;
+            font-weight: 300;
+            /* text-transform: uppercase; */
+        }
+    }
+`;
+
+const Follow = styled.div`
+    height: 300px;
+    background-color: #503b4a;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    transition: all 150ms ease-in;
+    > p {
+        padding-bottom: 50px;
+        font-size: 1.5rem;
+        font-weight: 500;
+        /* text-transform: uppercase; */
+    }
+    :hover {
+        filter: grayscale(100%);
+    }
+    @media (max-width: 700px) {
+        > p {
+            padding-top: 30px;
+            padding-bottom: 30px;
+            font-size: 1rem;
+            font-weight: 500;
+            /* text-transform: uppercase; */
+        }
+    }
+`;
+const ListItem = styled.div`
+    display: inline;
+    margin: 10px;
+    * {
+        color: white;
+    }
+    > a {
+        width: 100px;
+        height: 100px;
+    }
+    > a > div > svg {
+        margin: 10px;
+        width: 75% !important;
+        height: 75% !important;
+        text-align: center;
+        transition: all 150ms ease-in;
+    }
+    > a > div > svg:hover {
+        transform: scale(1.05);
+        opacity: 1;
+        /* box-shadow: 0px 5px 5px #111; */
+    }
+    @media (max-width: 700px) {
+        margin: 0px;
+        > a {
+            width: 60px !important;
+            height: 60px !important;
+        }
+    }
+`;
 
 export default function Contact() {
     return (
@@ -13,8 +161,8 @@ export default function Contact() {
                 <title>Contact | DevFlow</title>
             </Head>
             <NavBar />
-            <div className={styles.halves}>
-                <div className={styles.email}>
+            <Halves>
+                <Email>
                     <h1>Contact</h1>
                     <p>
                         Feel Free to contact this email for any business
@@ -23,56 +171,56 @@ export default function Contact() {
                     <h5>
                         <strong>Email:</strong> jkdelara@thedevflow.com
                     </h5>
-                </div>
-                <div className={styles.sponsor}>
+                </Email>
+                <Sponsors>
                     <h1>Sponsors</h1>
                     <p>
                         If you are interested in sponsoring DevFlow videos,
                         please email us for pricing and terms
                     </p>
-                </div>
-            </div>
-            <div className={styles.follow}>
+                </Sponsors>
+            </Halves>
+            <Follow>
                 <p>Follow me on these platforms</p>
                 <ul>
-                    <li className={styles.listItem}>
+                    <ListItem>
                         <SocialIcon
                             bgColor="#fff"
                             url="https://twitter.com/thedevflow"
                         />
-                    </li>
-                    <li className={styles.listItem}>
+                    </ListItem>
+                    <ListItem>
                         <SocialIcon
                             bgColor="#fff"
                             url="https://facebook.com/thedevflow"
                         />
-                    </li>
-                    <li className={styles.listItem}>
+                    </ListItem>
+                    <ListItem>
                         <SocialIcon
                             bgColor="#fff"
                             url="https://instagram.com/thedevflow"
                         />
-                    </li>
-                    <li className={styles.listItem}>
+                    </ListItem>
+                    <ListItem>
                         <SocialIcon
                             bgColor="#fff"
                             url="https://youtube.com/channel/UCWzJk743eDpkpQaVbY2YsPw"
                         />
-                    </li>
-                    <li className={styles.listItem}>
+                    </ListItem>
+                    <ListItem>
                         <SocialIcon
                             bgColor="#fff"
                             url="https://linkedin.com/jkdelara"
                         />
-                    </li>
-                    <li className={styles.listItem}>
+                    </ListItem>
+                    <ListItem>
                         <SocialIcon
                             bgColor="#fff"
                             url="https://github.com/xjkbro"
                         />
-                    </li>
+                    </ListItem>
                 </ul>
-            </div>
+            </Follow>
             <Footer />
         </div>
     );
